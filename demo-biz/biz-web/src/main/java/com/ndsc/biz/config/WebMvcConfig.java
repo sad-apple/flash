f6447019f564082dbdc911aeced9253abe1b887b
zsp <614272936@qq.com>
2023-04-04 17:39:24 +0800

    新增跨域配置

--- demo-biz/biz-web/src/main/java/com/ndsc/biz/config/WebMvcConfig.java
+++ demo-biz/biz-web/src/main/java/com/ndsc/biz/config/WebMvcConfig.java
@@ -1,5 +1,11 @@
 package com.ndsc.biz.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -7,10 +13,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author zsp
  * @date 2023/4/4 9:28
  */
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
     }
 
+    /**
+     * todo 跨域配置，待验证
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //是否允许请求带有验证信息
+        corsConfiguration.setAllowCredentials(true);
+        // 允许访问的客户端域名
+        corsConfiguration.addAllowedOriginPattern("*");
+        // 允许服务端访问的客户端请求头
+        corsConfiguration.addAllowedHeader("*");
+        // 允许访问的方法名,GET POST等
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 }
