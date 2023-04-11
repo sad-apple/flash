d83add8add7c5dfcac37593c1b62effa75c89af6
zsp <614272936@qq.com>
2023-04-11 11:17:05 +0800

    修复

--- demo-biz/biz-dao/src/main/java/com/ndsc/biz/dao/config/MybatisPlusConfig.java
+++ demo-biz/biz-dao/src/main/java/com/ndsc/biz/dao/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package com.ndsc.biz.dao.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
@@ -21,7 +22,7 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         // 分页拦截器
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         // 乐观锁拦截器
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
--- demo-biz/biz-web/src/main/java/com/ndsc/biz/controller/BizUserController.java
+++ demo-biz/biz-web/src/main/java/com/ndsc/biz/controller/BizUserController.java
@@ -1,9 +1,12 @@
 package com.ndsc.biz.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import com.github.xiaoymin.knife4j.annotations.ApiSort;
 import com.ndsc.biz.dao.entity.BizUser;
 import com.ndsc.biz.service.user.BizUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zsp
  * @date 2023/4/4 16:36
  */
+@Api(tags = "用户管理")
+@ApiSort(12000)
 @RestController
 public class BizUserController {
 
     @Autowired
     private BizUserService userService;
 
+    @ApiOperationSupport(order = 12001)
+    @ApiOperation(value = "新增用户")
     @PostMapping("/biz-users")
     public boolean insert(@RequestBody BizUser sysUser) {
         userService.save(sysUser);
