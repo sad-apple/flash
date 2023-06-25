8a94142c23d36e58c7df8bf8c10af63ae00b50d8
zsp <614272936@qq.com>
2023-06-25 19:57:41 +0800

    1、新增springcloud

---
+++ examples/flash-cloud/cloud-gateway/src/main/java/com/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,36 @@
+package com.cloud.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author zsp
+ * @date 2023/6/25 16:43
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@Slf4j
+public class GatewayApplication {
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(GatewayApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port", "8080");
+        String path = env.getProperty("server.servlet.context-path");
+        path = path == null ? "" : path;
+        log.info("\n----------------------------------------------------------\n\t" +
+                 "Application demo is running! Access URLs:\n\t" +
+                 "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                 "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                 "Swagger doc url: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                 "----------------------------------------------------------");
+    }
+
+}
